19565a2d6633d62e6ee698f489462c3031a749e0
Poorani Ganesan <pooranig.it@gmail.com>
2025-03-20 14:21:23 +0530

    Test case 8: raise error when negatives including numbers are in string)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,7 @@
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
-        negatives = numbers.scan(/-\d+/)
-        if !negatives.empty?
-            raise "negatives not allowed #{negatives.join(',')}"
-        end
+        validate_negatives(numbers)
         if numbers.start_with?("//")
             computed_str = numbers.split("\\n")
             delimiter = computed_str[0].gsub("//", "")
@@ -12,5 +9,13 @@ class StringCalculator
             numbers=numbers.gsub(delimiter, ",")
         end
         numbers.gsub("\\n", ", ").split(",").sum(&:to_i)
+    end
+
+    private
+    def validate_negatives(numbers)
+        negatives = numbers.scan(/-\d+/)
+        if !negatives.empty?
+            raise "negatives not allowed #{negatives.join(',')}"
+        end
     end
 end
