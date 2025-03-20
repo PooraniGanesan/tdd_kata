d2d5f5c4cfdc7e4a7733dfe07da941968157a925
Poorani Ganesan <pooranig.it@gmail.com>
2025-03-20 12:57:38 +0530

    Test case 3: return sum of unknown amount of numbers in a string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe StringCalculator do
     it 'should return sum of numbers passed in a string with comma delimiter' do
       expect(StringCalculator.new.add('2,3')).to eq(5)
     end
+
+    it 'should return sum of unknown amount of numbers in a string' do
+      expect(StringCalculator.new.add('10, 10, 10, 10, 10, 10, 10, 10, 10, 10')).to eq(100)
+
+    end
 end
