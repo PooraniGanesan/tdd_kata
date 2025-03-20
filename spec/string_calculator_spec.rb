f01fe000f10ba993032f1fdf04503576178a3cc1
Poorani Ganesan <pooranig.it@gmail.com>
2025-03-20 12:54:55 +0530

    Test case 2: return sum of minimal numbers in a string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe StringCalculator do
     it 'should return 0 if the string is empty' do
       expect(StringCalculator.new.add('')).to eq(0)
     end
+
+    it 'should return sum of numbers passed in a string with comma delimiter' do
+      expect(StringCalculator.new.add('2,3')).to eq(5)
+    end
 end
