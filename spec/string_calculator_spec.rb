d1c239b2f2b8e674ca22f9e45a7b136fb87256b4
Poorani Ganesan <pooranig.it@gmail.com>
2025-03-20 13:06:55 +0530

    Test case 5: return sum of of numbers in a string with different delimiters(FAIL)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe StringCalculator do
     it 'should return sum of numbers with new line delimiter' do
       expect(StringCalculator.new.add('1\n2,3')).to eq(6)
     end
+
+    it 'should return sum of numbers with differnt delimiter specified at the start of the string' do
+      expect(StringCalculator.new.add('//;\n1;2')).to eq(3)
+    end
 end
